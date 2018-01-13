fc09ccb44a7a4aab787791fbf9221e143a95edbe
Rafael Fritsch <r.fritsch@campus.tu-berlin.de>
2018-01-13 14:49:13 +0100

    fixed point handling

--- planet.js
+++ planet.js
@@ -12,8 +12,8 @@ class Moon {
     }
     
     getCurrentPosition() {
-        var offset = this.distance * new Phaser.Point(Math.cos(this.angle), Math.sin(this.angle));
-        return this.planet.sprite.body.position + offset;
+        let offset = new Phaser.Point(this.distance * Math.cos(this.angle), this.distance * Math.sin(this.angle));
+        return Phaser.Point.add( this.planet.sprite.body.position, offset);
     }
     
     update() {
@@ -25,7 +25,7 @@ class Moon {
 class Planet {
 
     constructor(image, pos, _moons) {
-        this.sprite = game.add.sprite(pos[0], pos[1], image);
+        this.sprite = game.add.sprite(pos.x, pos.y, image);
         this.sprite.anchor.set(0.5);
         game.physics.arcade.enable(this.sprite);
         
@@ -33,7 +33,6 @@ class Planet {
         let dist = 100;
         let angle = 2*3.14 / _moons.length;
         for(let i = 0; i < _moons.length; ++i) {
-            console.log("asd");
             this.moons.push( new Moon(_moons[i], this, dist*(i+1), angle*i) );
         }
     }
