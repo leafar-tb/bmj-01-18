1e06069140b299c5b668e601de6d36fcdf164b8d
Rafael Fritsch <r.fritsch@campus.tu-berlin.de>
2018-01-13 13:43:03 +0100

    phaser callbacks

---
+++ main.js
@@ -0,0 +1,17 @@
+var game = new Phaser.Game(800, 600, Phaser.AUTO, 'bmj-01-18', { preload: preload, create: create, update: update, render: render });
+
+function preload() {
+
+}
+
+function create() {
+
+}
+
+function update () {
+
+}
+
+function render () {
+
+}
