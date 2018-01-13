b0824a4eaff5f4118001cf0129796d98795b2ad4
Adina Johnson <adinacjohnson@gmail.com>
2018-01-13 16:25:30 +0100

    scrolling world

--- src/Game.js
+++ src/Game.js
@@ -11,10 +11,14 @@ BasicGame.Game = function (game) {
 BasicGame.Game.prototype = {
 
     create: function () {
+
+        game.world.resize(3000, 3000);
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
         let worldCenter = new Phaser.Point(game.world.centerX, game.world.centerY);
         PLAYER = new Planet('player', worldCenter, ['moon1']);
+        game.physics.enable(PLAYER.sprite);
+        game.camera.follow(PLAYER.sprite, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
     },
 
     update: function () {
@@ -22,9 +26,9 @@ BasicGame.Game.prototype = {
         if (game.physics.arcade.distanceToPointer(PLAYER.sprite, game.input.activePointer) > 8) {
             game.physics.arcade.moveToPointer(PLAYER.sprite, 300);
         } else { // close enough
-            PLAYER.sprite.body.velocity.set(0);    
+            PLAYER.sprite.body.velocity.set(0);
         }
-        
+
         PLAYER.update();
     },
 
